615623d44be0c688066ae619d1500a74aa8d3651
griffinjoshs <leveloutusa@gmail.com>
2023-02-08 18:13:49 -0500

    navbar changes

--- public/assets/js/changeBackgroundColorMode.js
+++ public/assets/js/changeBackgroundColorMode.js
@@ -9,16 +9,24 @@ document.documentElement.setAttribute('data-bs-theme', theme);
 localStorage.setItem('theme', theme);
 }
 
-toggleBgColor.addEventListener('change', () => {
-if (toggleBgColor.checked) {
-setTheme('dark');
-} else {
-setTheme('light');
-}
-});
+
+    const themeChanger = () => {
+        if (toggleBgColor.checked) {
+            setTheme('dark');
+            document.documentElement.style.setProperty('--main-color', 'black');
+          } else {
+            setTheme('light');
+            document.documentElement.style.setProperty('--main-color', 'white');
+          }
+    }
+
+    toggleBgColor.addEventListener('change', themeChanger)
+  
 
 if (storedTheme) {
 document.documentElement.setAttribute('data-bs-theme', storedTheme);
 toggleBgColor.checked = storedTheme === 'dark';
 }
 // }
+
+themeChanger()
